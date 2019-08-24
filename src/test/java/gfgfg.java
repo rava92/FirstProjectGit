96bc2a4b1710cf43c045727dcd4538f9885bd10e
rava92 <abilkadirov.ra@gmail.com>
2019-08-23 20:46:18 -0400

    dfdfdfdfd

---
+++ src/test/java/gfgfg.java
@@ -0,0 +1,8 @@
+public class gfgfg {
+    public static void main(String[] args) {
+        System.out.println("rrrrrr'");
+        System.out.println("rrrrrr'");
+        System.out.println("rrrrrr'");
+        System.out.println("rrrrrr'");
+    }
+}
